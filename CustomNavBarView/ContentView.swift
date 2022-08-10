6e1f503531b4b6331f006d2b65ba4768f42e10ac
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-10 11:47:08 +0200

    Initial Commit

---
+++ CustomNavBarView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomNavBarView
+//
+//  Created by Skorobogatow, Christian on 10/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomNavBarView/CustomNavBarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarViewApp.swift
+//  CustomNavBarView
+//
+//  Created by Skorobogatow, Christian on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
